ddbea80a555d792bf077de6330b9df9a56ae1473
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-11-12 14:51:58 -0500

    Add project files.

---
+++ GenericMethodsAndClasses/Elems.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethodsAndClasses
+{
+    internal class Elems <T>
+    {
+       
+        //make this class generic
+        
+        
+            public T[] elements = new T[3];
+
+            public void Add(T elem1, T elem2, T elem3)
+            {
+                elements[0] = elem1;
+                elements[1] = elem2;
+                elements[2] = elem3;
+            }
+
+            public void Show()
+            {
+                foreach (T item in elements)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+    }
+
+
---
+++ GenericMethodsAndClasses/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace GenericMethodsAndClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> intStack = new Stack<int>();
+            intStack.Push(3);
+            intStack.Push(6);
+            intStack.Push(7);
+
+            Console.WriteLine(intStack.Get(1));
+
+
+            Elems<string> elems1 = new Elems<string>();
+            elems1.Add("John", "Tamara", "David");
+            elems1.Show();
+
+            Console.WriteLine();
+
+            Elems<int> elems2 = new Elems<int>();
+            elems2.Add(5, 14, 13);
+            elems2.Show();
+        }
+    }
+}
